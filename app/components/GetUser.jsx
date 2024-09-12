f103ed73bc84eb8ae1a630f2e2b72f9f12754bc1
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2024-09-12 18:33:09 +0200

    Deleted Status in GetUser component

--- app/components/GetUser.jsx
+++ app/components/GetUser.jsx
@@ -51,7 +51,6 @@ export default function Home() {
           <p>Total experience: {playerData.exp}</p>
           <p>Efficient hours played: {playerData.ehp}</p>
           <p>Efficient hours bossed: {playerData.ehb}</p>
-          <p>Status:: {playerData.status}</p>
         </div>
       )}
     </div>

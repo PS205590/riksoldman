a6df30e2d5ae681eacf5c762781de4114f85b896
Rik Brandenburg <93656269+PS205590@users.noreply.github.com>
2024-10-08 10:58:02 +0200

    Removed local fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,17 +4,6 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import { ThemeProvider } from "../components/theme-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "Riksoldman",
   description: "A wiseoldman clone for College, with the unique name of Riksoldman.",
@@ -28,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <ThemeProvider
             attribute="class"
